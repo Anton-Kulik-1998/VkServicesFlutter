fbb153a40f59adff6c44ec077bd15b80d93ec521
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-03 21:33:43 +0300

    Создан демо экран с данными

---
+++ lib/features/vk_services/view/vk_services_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:vk_services_flutter/repositories/vk_services/data_example.dart';
+import 'package:vk_services_flutter/repositories/vk_services/models/models.dart';
+
+class HomeScreen extends StatelessWidget {
+  final DataService _dataService = DataService();
+
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<VKDataModel>(
+        future: _dataService.fetchVKData(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final vkData = snapshot.data!;
+            return ListView.builder(
+              itemCount: vkData.body.services.length,
+              itemBuilder: (context, index) {
+                final service = vkData.body.services[index];
+                return ListTile(
+                  title: Text(service.name),
+                  subtitle: Text(service.description),
+                  onTap: () {
+                    // Действие при нажатии на сервис
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
