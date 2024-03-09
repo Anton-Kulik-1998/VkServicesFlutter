f956e794f3ccecc12742d1f7cb713128a480cbe3
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-09 16:33:16 +0300

    Исправление цветовой палитры темы

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -6,7 +6,7 @@ ThemeData darkTheme = ThemeData(
     background: Colors.grey.shade900,
     primary: Colors.grey.shade800,
     secondary: Colors.grey.shade700,
-    inversePrimary: Colors.grey.shade800,
+    inversePrimary: Colors.grey.shade400,
   ),
   textTheme: ThemeData.dark().textTheme.apply(
         bodyColor: Colors.grey[300],
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-      background: Colors.grey.shade300,
-      primary: Colors.grey.shade200,
-      secondary: Colors.grey.shade400,
-      inversePrimary: Colors.grey.shade800),
-      
+      background: Colors.grey.shade100,
+      primary: Colors.white,
+      secondary: Colors.grey.shade100,
+      inversePrimary: Colors.grey.shade400),
   textTheme: ThemeData.light().textTheme.apply(
         bodyColor: Colors.grey[800],
         displayColor: Colors.black,
